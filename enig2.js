6dfcb8eff16b64dd1c9796fc8dbc4215d19d7fd0
Eyshila <eyshilaneubertbassetto@gmail.com>
2023-12-07 16:03:27 -0300

    Update enig2.js

--- enig2.js
+++ enig2.js
@@ -4,10 +4,8 @@ function verificarResposta() {
     console.log("Resposta Digitada:", respostaDigitada);  // Adicione esta linha
 
     // Substitua 'resposta_correta' pela resposta correta do enigma
-    if (respostaDigitada === 'Vão e procurem entender o que quer dizer este trecho das Escrituras Sagradas: “Eu quero que as pessoas sejam bondosas e não que me ofereçam sacrifícios de animais.” Porque eu vim para chamar os pecadores e não os bons.') {
+    if (respostaDigitada === 'Mateus 9:13') {
         console.log("Resposta Correta!");  // Adicione esta linha
         window.location.href = 'eng2p.html';
     } else {
-        alert('Resposta incorreta. Tente novamente.');
-    }
-}
+        alert('Resposta incorreta. Tente novamente.')
