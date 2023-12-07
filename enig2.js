79ce931a67b893fce56311dc91c9cce6c6be90e6
Eyshila <eyshilaneubertbassetto@gmail.com>
2023-12-07 16:10:36 -0300

    Update enig2.js

--- enig2.js
+++ enig2.js
@@ -6,6 +6,6 @@ function verificarResposta() {
     // Substitua 'resposta_correta' pela resposta correta do enigma
     if (respostaDigitada === 'Mateus 9:13') {
         console.log("Resposta Correta!");  // Adicione esta linha
-        window.location.href = 'eng2p.html';
+        window.location.href = 'enig2p.html';
     } else {
         alert('Resposta incorreta. Tente novamente.')
