243343695e9a603d1231660479093700066269f8
Eyshila <eyshilaneubertbassetto@gmail.com>
2023-12-08 12:26:14 -0300

    Update inig1.js

--- inig1.js
+++ inig1.js
@@ -4,7 +4,7 @@ function verificarResposta() {
     console.log("Resposta Digitada:", respostaDigitada);  // Adicione esta linha
 
     // Substitua 'resposta_correta' pela resposta correta do enigma
-    if (respostaDigitada === 'Pedro') {
+    if (respostaDigitada === 'pedro') {
         console.log("Resposta Correta!");  // Adicione esta linha
         window.location.href = 'inig1p.html';
     } else {
