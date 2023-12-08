d606a63597ab9dc41e822d9585099d9bce0ee079
Eyshila <eyshilaneubertbassetto@gmail.com>
2023-12-08 11:44:02 -0300

    Add files via upload

---
+++ enig1.js
@@ -0,0 +1,14 @@
+function verificarResposta() {
+    var respostaDigitada = document.getElementById("resposta").value.toLowerCase();
+
+    console.log("Resposta Digitada:", respostaDigitada);  // Adicione esta linha
+
+    // Substitua 'resposta_correta' pela resposta correta do enigma
+    if (respostaDigitada === 'Jonas') {
+        console.log("Resposta Correta!");  // Adicione esta linha
+        window.location.href = 'enig1p.html';
+    } else {
+        alert('Resposta incorreta. Tente novamente.');
+    }
+}
+ 
---
+++ inig1.js
@@ -0,0 +1,13 @@
+function verificarResposta() {
+    var respostaDigitada = document.getElementById("resposta").value.toLowerCase();
+
+    console.log("Resposta Digitada:", respostaDigitada);  // Adicione esta linha
+
+    // Substitua 'resposta_correta' pela resposta correta do enigma
+    if (respostaDigitada === 'Pedro') {
+        console.log("Resposta Correta!");  // Adicione esta linha
+        window.location.href = 'inig1p.html';
+    } else {
+        alert('Resposta incorreta. Tente novamente.');
+    }
+}
