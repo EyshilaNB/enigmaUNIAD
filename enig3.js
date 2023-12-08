c5ebf6e5905e633007426d25c138af00ce2da252
Eyshila <eyshilaneubertbassetto@gmail.com>
2023-12-08 12:13:57 -0300

    Create enig3.js

---
+++ enig3.js
@@ -0,0 +1,13 @@
+function verificarResposta() {
+    var respostaDigitada = document.getElementById("resposta").value.toLowerCase();
+
+    console.log("Resposta Digitada:", respostaDigitada);  // Adicione esta linha
+
+    // Substitua 'resposta_correta' pela resposta correta do enigma
+    if (respostaDigitada === 'buraco') {
+        console.log("Resposta Correta!");  // Adicione esta linha
+        window.location.href = 'enig3p.html';
+    } else {
+        alert('Resposta incorreta. Tente novamente.');
+    }
+}
